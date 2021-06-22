53e78bdac143618f37e81605a4d6f56612ea4e7a
henryque <henryque_1@hotmail.com>
2021-06-22 15:13:24 -0300

    chore: add typeorm, reflect-metadata and sqlite

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,6 @@
+import "reflect-metadata";
 import express from "express";
 
 const app = express();
-
-app.get('/', (req, res) => {
-  return res.send('Hey, everything is working!')
-});
 
 app.listen('4000', () => console.log('Server is running!'));
