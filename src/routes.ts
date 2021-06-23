861d21eac23ea482ec869fa087798cf684ed80aa
henryque <henryque_1@hotmail.com>
2021-06-23 09:26:15 -0300

    feat: add CreateTagController and its routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
+import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
 
 router.get('/', (req, res) => {
   return res.send('GET found!');
 });
 
 router.post('/users', createUserController.handle);
+router.post('/tags', createTagController.handle);
 
 export { router }
