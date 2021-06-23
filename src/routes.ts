7ec003ea34bf733572a20aa87bf57e159719580d
henryque <henryque_1@hotmail.com>
2021-06-23 19:36:08 -0300

    feat: create and add  ensureAdmin middlewares to be used in tags routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,16 @@
 import { Router } from "express";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
 
-router.get('/', (req, res) => {
-  return res.send('GET found!');
-});
+router.use(ensureAdmin);
 
+router.post('/tags', ensureAdmin, createTagController.handle);
 router.post('/users', createUserController.handle);
-router.post('/tags', createTagController.handle);
 
 export { router }
