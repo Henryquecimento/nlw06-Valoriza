1dc04442541f85cee7c3f7888ba65a64abc6b6b6
henryque <henryque_1@hotmail.com>
2021-06-25 20:04:12 -0300

    refactor(ensureAdmin): capturing user_id from request

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,9 +1,16 @@
 import { NextFunction, Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { UsersRepositories } from "../repositories/UsersRepositories";
 
 
-export function ensureAdmin (request: Request, response: Response, next: NextFunction) {
+export async function ensureAdmin (request: Request, response: Response, next: NextFunction) {
+  const { user_id } = request;
+ 
+  const usersRepositories = getCustomRepository(UsersRepositories);
+
+  const { admin } = await usersRepositories.findOne(user_id);
+ 
   // CHECK if Admin user
-  const admin = true;
 
   if (admin) {
     return next();
