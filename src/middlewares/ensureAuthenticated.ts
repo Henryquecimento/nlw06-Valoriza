29b98cf613b1cc2fa95d5c6a1b9f74d4deecc51c
henryque <henryque_1@hotmail.com>
2021-06-25 20:02:38 -0300

    style(ensureAuthenticated): adding some spaces

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,4 +1,3 @@
-
 import { NextFunction, Request, response, Response } from "express";
 import { verify } from "jsonwebtoken";
 
@@ -6,7 +5,11 @@ interface IPayload {
   sub: string;
 }
 
-export function ensureAuthenticated (request: Request, respose: Response, next: NextFunction ) {
+export function ensureAuthenticated(
+  request: Request,
+  respose: Response,
+  next: NextFunction
+) {
   //Reveive Token
   const authtoken = request.headers.authorization;
 
@@ -15,27 +18,23 @@ export function ensureAuthenticated (request: Request, respose: Response, next: 
     return respose.status(401).end();
   }
 
-  const [,token] = authtoken.split(" ") 
+  const [, token] = authtoken.split(" ");
   /* Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9... -> Baerer (space) token
-  */
+   */
 
   try {
     //Validate if token is valid
     const { sub } = verify(
-      token, 
+      token,
       "ac0b33d67cff472e1616d7525fbe6f14"
-     ) as IPayload 
-     /*"ac0b33d67cff472e1616d7525fbe6f14" from Authenticate service*/
-    
+    ) as IPayload;
+    /*"ac0b33d67cff472e1616d7525fbe6f14" from Authenticate service*/
+
     //Get user information
     request.user_id = sub;
 
     return next();
-
   } catch (err) {
     return response.status(401).end();
   }
-
-
-  
 }
